2eab8ecf182ffc71de6264c87d3109991905f966
DESKTOP-DE5672F\Aguinaldo <raguinaldoj@indracompany.com>
2021-09-30 01:11:24 +0800

    adding https

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const rewardListRoute = require('./routes/rewardListRoute');
+const https = require('https')
 require('dotenv/config');
 
 app.use(express.urlencoded({extended: true})); 
@@ -20,4 +21,7 @@ mongoose.connect(process.env.DB_CONNECTION, (error) => {
     console.log(error ? error : 'db connected!');
 });
 
-app.listen(process.env.PORT || 3000);
+https.createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
+  }, app).listen(process.env.PORT || 3000);
